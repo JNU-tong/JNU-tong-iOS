25706e1bc474623380f83c2a42454015f26ca3ba
jw <hjw01234@gmail.com>
2017-09-17 11:05:35 +0900

    브랜치 정리

---
+++ JNU-tong/JNU-tong/SchoolBusTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SchoolBusTableViewCell.swift
+//  JNU-tong
+//
+//  Created by mac on 2017. 9. 15..
+//  Copyright © 2017년 tong. All rights reserved.
+//
+
+import UIKit
+
+class SchoolBusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
