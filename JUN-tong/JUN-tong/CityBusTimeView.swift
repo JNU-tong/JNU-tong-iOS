88ff09aff3b71deaba99142a323ae60577c82779
Seong ho Hong <grohong76@gmail.com>
2017-10-01 21:46:00 +0900

    tableCell add

---
+++ JUN-tong/JUN-tong/CityBusTimeView.swift
@@ -0,0 +1,13 @@
+//
+//  CityBusTimeView.swift
+//  JUN-tong
+//
+//  Created by Seong ho Hong on 2017. 10. 1..
+//  Copyright © 2017년 Seong ho Hong. All rights reserved.
+//
+
+import UIKit
+
+class CityBusTimeView: UIViewController {
+    
+}
