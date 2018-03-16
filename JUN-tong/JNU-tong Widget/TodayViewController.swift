8e69b0aa80f7c0ee8be2b2b18d3a6d6d7db43fa0
Seong ho Hong <grohong76@gmail.com>
2018-03-16 20:23:50 +0900

    [feature]: set update time

--- JUN-tong/JNU-tong Widget/TodayViewController.swift
+++ JUN-tong/JNU-tong Widget/TodayViewController.swift
@@ -10,25 +10,32 @@ import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+    @IBOutlet var updateTimeLabel: UILabel!
+    let updateDataFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+        updateTimeLabel.text = ""
+        setUpdateTime()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
         
         completionHandler(NCUpdateResult.newData)
     }
     
+    private func setUpdateTime() {
+        let updateData = Date()
+        updateDataFormatter.dateFormat = "yyyy.MM.dd HH:mm"
+        let updateTimeResult = updateDataFormatter.string(from: updateData)
+        updateTimeLabel.text = updateTimeResult
+    }
+    
+    @IBAction func updateButton(_ sender: Any) {
+        setUpdateTime()
+    }
 }
